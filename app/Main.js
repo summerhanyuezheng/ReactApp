e1b0c129239f0d7bb849cea794961f9b9809619c
summerhanyuezheng <summer199303@gmail.com>
2023-06-15 15:24:51 -0700

    added command script for webpack in package.json;added HMR in main.js; added test code in main.js;tested workflow in localhost3000, it works now

--- app/Main.js
+++ app/Main.js
@@ -1,2 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>This is our app!!</h1>
+      <p>Had burger for lunch</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<ExampleComponent />)
+
+if (module.hot) {
+  module.hot.accept()
+}
